62f8c0eea339644ee5ffd5d54c3bb007d79d98d3
David Hällgren <daha2502@student.miun.se>
2025-11-04 22:58:25 +0100

    Laggt till If statment i slutsattsen

--- Yatzy/MainWindow.xaml.cs
+++ Yatzy/MainWindow.xaml.cs
@@ -21,18 +21,31 @@ namespace Yatzy
             InitializeComponent();
         }
         private void btnSummering_Click(object sender, RoutedEventArgs e){
-            int summaEtt = int.Parse(txtBoxEtt.Text);
-            int summaTvå = int.Parse(txtBoxTvå.Text);
-            int summaTre = int.Parse(txtBoxTre.Text);
-            int summaFyra = int.Parse(txtBoxFyra.Text);
-            int summaFem = int.Parse(txtBoxFem.Text);
-            int summaSex = int.Parse(txtBoxSex.Text);
 
-            int points = summaEtt + summaTvå + summaTre + summaFyra + summaFem + summaSex;
-            String Sum = points.ToString();
-            // string Points =  summaEtt + summaTvå + summaTre + summaFyra + summaFem + summaSex;
+            int summaEtt = int.Parse(txtBoxEtt.Text);
+                                                                                                // Gör om variable "Text" till int (nummer) och sparar i "SummaEtt"
+            int summaTvå = int.Parse(txtBoxTvå.Text);
             
-            MessageBox.Show(Sum);
+            int summaTre = int.Parse(txtBoxTre.Text);
+            
+            int summaFyra = int.Parse(txtBoxFyra.Text);
+            
+            int summaFem = int.Parse(txtBoxFem.Text);
+            
+            int summaSex = int.Parse(txtBoxSex.Text);
+            
+            int points = summaEtt + summaTvå + summaTre + summaFyra + summaFem + summaSex;      // Summerar och lägger värdet i "points"
+
+            if (points >= 63){                                                                  // Om värdet i points är större eller lika med 63
+                //string message = $"Du fick {points} och får därför YATZY bonus!";
+                //MessageBox.Show(message);
+                txtSum.Text = $"Du fick {points} och får därför YATZY bonus!";                  // Anropar "txtSum" som är ett TextBlock och lägger in angiven text i "Text"
+            }
+            else{                                                                               // Allt annat
+                 //string message = $"Du fick {points} och får därför inte YATZY bonus";         
+                 //MessageBox.Show(message);                                                     
+                 txtSum.Text = $"Du fick {points} och får därför inte YATZY bonus";             // Anropar "txtSum" som är ett TextBlock och lägger in angiven text i "Text"
+            }
         }
     }
 }
