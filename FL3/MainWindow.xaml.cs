68526672d5f642821f55aa8a3989258f2b6d474a
David Hällgren <daha2502@student.miun.se>
2025-11-05 16:13:42 +0100

    If statements

---
+++ FL3/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System.Numerics;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FL3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btbCalculate_Click(object sender, RoutedEventArgs e)
+        {
+            // Vi vill summera alla värden på yatzy
+            // övre halvan,1, ,2 ,3 ,4, ,5 och sex
+            // Vad är det vi behöver för att kunna beräkna summan?
+            // Användaren matar in heltal 1 -> 6
+            // Summan av flera heltal blir alltid ett heltal
+
+            int ones = 0, twos = 0, threes = 0, fours = 0, fives = 0, sixes = 0, total;    // Går att lägga in flera variabler av samma typ (int = nummer)
+
+            // Vi vill hämta värden från användaren, inte hård koda 
+
+            // vi kan inte stoppa in tal i en sträng
+            // Låt oss konvertera --> parse
+            // Det är en metod. Akka netider gar alltid en parantes
+            // ibland ska det stå något inom parantesen
+            ones = int.Parse(txtBoxEtt.Text);
+            twos = int.Parse(txtBoxEtt.Text);
+            threes = 
+
+            total = ones + twos + threes + fours + fives + sixes;
+            txtBlockSumma.Text = $"Du fick {total} poäng!";
+
+
+        }
+    }
+}
--- Yatzy/MainWindow.xaml.cs
+++ Yatzy/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Numerics;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -21,31 +22,75 @@ namespace Yatzy
             InitializeComponent();
         }
         private void btnSummering_Click(object sender, RoutedEventArgs e){
+           int summaEtt = 0, summaTvå = 0, summaTre = 0, summaFyra = 0, summaFem = 0, summaSex = 0;
+           string err = "";                                                                    //Skapar string "err" utan värde för att ge den värde om scrips ska avslutas
+           if (int.Parse(txtBoxEtt.Text) >= 1 && int.Parse(txtBoxEtt.Text) <= 5) {
+                summaEtt = int.Parse(txtBoxEtt.Text) * 1;
+                string totalEtt = summaEtt.ToString();
+                txtBlockTotalEtt.Text = totalEtt;
+           }
+           else {
+                txtBlockTotalEtt.Text = "Du kasta för få/många tärningar";
+                err = "err";
+           }
+           if (int.Parse(txtBoxTvå.Text) >= 1 && int.Parse(txtBoxTvå.Text) <= 5) {
+                summaTvå = int.Parse(txtBoxTvå.Text) * 2;
+                string totalTvå = summaTvå.ToString();
+                txtBlockTotalTvå.Text = totalTvå;
+           }
+           else {
+                txtBlockTotalTvå.Text = "Du kasta för få/många tärningar";
+                err = "err";
+           }
+           if (int.Parse(txtBoxTre.Text) >= 1 && int.Parse(txtBoxTre.Text) <= 5) {
+                summaTre = int.Parse(txtBoxTre.Text) * 3;
+                string totalTre = summaTre.ToString();
+                txtBlockTotalTre.Text = totalTre;
+           }
+           else {
+                txtBlockTotalTre.Text = "Du kasta för få/många tärningar";
+                err = "err";
+           }
+           if (int.Parse(txtBoxFyra.Text) >=1 && int.Parse(txtBoxFyra.Text) <= 5) {
+                summaFyra = int.Parse(txtBoxFyra.Text) * 4;
+                string totalFyra = summaFyra.ToString();
+                txtBlockTotalFyra.Text = totalFyra;
+           }
+           else {
+                txtBlockTotalFyra.Text = "Du kasta för få/många tärningar";
+                err = "err";
+           }
+           if (int.Parse(txtBoxFem.Text) >= 1 && int.Parse(txtBoxFem.Text) <= 5) {
+                summaFem = int.Parse(txtBoxFem.Text) * 5;
+                string totalFem = summaFem.ToString();
+                txtBlockTotalFem.Text = totalFem;
+           }
+           else {
+                txtBlockTotalFem.Text = "Du kasta för få/många tärningar";
+                err = "err";
+           }
+           if (int.Parse(txtBoxSex.Text) >= 1 && int.Parse(txtBoxSex.Text) <= 5) {
+                summaSex = int.Parse(txtBoxSex.Text) * 6;
+                string totalSex = summaSex.ToString();
+                txtBlockTotalSex.Text = totalSex;
+           }
+           else {
+                txtBlockTotalSex.Text = "Du kasta för få/många tärningar";
+                err = "err";
+           }
+           if (err == "err") {                                                                 // Om err har värde "err" avbryter script
+                txtBlockSum.Text = "";                                                         // Nollställer variable om man gör fel
+                return;                                                                        // Avbryter sent för att alla fel ska pressenteras
+           }
+           int summa = summaEtt + summaTvå + summaTre + summaFyra + summaFem + summaSex;       // Summerar och lägger värdet i "points"
 
-            int summaEtt = int.Parse(txtBoxEtt.Text);
-                                                                                                // Gör om variable "Text" till int (nummer) och sparar i "SummaEtt"
-            int summaTvå = int.Parse(txtBoxTvå.Text);
-            
-            int summaTre = int.Parse(txtBoxTre.Text);
-            
-            int summaFyra = int.Parse(txtBoxFyra.Text);
-            
-            int summaFem = int.Parse(txtBoxFem.Text);
-            
-            int summaSex = int.Parse(txtBoxSex.Text);
-            
-            int points = summaEtt + summaTvå + summaTre + summaFyra + summaFem + summaSex;      // Summerar och lägger värdet i "points"
-
-            if (points >= 63){                                                                  // Om värdet i points är större eller lika med 63
-                //string message = $"Du fick {points} och får därför YATZY bonus!";
-                //MessageBox.Show(message);
-                txtSum.Text = $"Du fick {points} och får därför YATZY bonus!";                  // Anropar "txtSum" som är ett TextBlock och lägger in angiven text i "Text"
-            }
-            else{                                                                               // Allt annat
-                 //string message = $"Du fick {points} och får därför inte YATZY bonus";         
-                 //MessageBox.Show(message);                                                     
-                 txtSum.Text = $"Du fick {points} och får därför inte YATZY bonus";             // Anropar "txtSum" som är ett TextBlock och lägger in angiven text i "Text"
-            }
+           if (summa >= 63){                                                                   // Om värdet i points är större eller lika med 63
+                summa += 50;                                                                   // lägger till 50 "+="
+                txtBlockSum.Text = $"dina totala poäng är {summa} poäng med YAZTY BONUS!";     // Anropar "txtSum" som är ett TextBlock och lägger in angiven text i "Text"
+           }
+           else{                                                                               // Allt annat                                                    
+                txtBlockSum.Text = $"Du fick bara {summa} poäng och bör inte spela YATZY mer";  // Anropar "txtSum" som är ett TextBlock och lägger in angiven text i "Text"
+           }
         }
     }
 }
