f5685c57d33e07ce28ee1d1876e88ae4465f0fd0
David Hällgren <daha2502@student.miun.se>
2025-11-25 15:59:09 +0100

    Börjat med classer

---
+++ Class/Frukt.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class
+{
+    public class Frukt
+    {
+        public string Färg { get; set; }
+        public string Smak { get; set; }
+        public double Vikt { get; set; }
+        public double Storlek { get; set; }
+
+
+
+    }
+}
---
+++ Class/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Text;
+using System.Windows;
+
+
+namespace Class
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            List<Frukt> frukskål = new List<Frukt>();
+            
+            Apelsin apelsin = new Apelsin
+            {
+                Färg = "Orange",
+                Vikt = 0.25,
+                Storlek = 4,
+
+                Smak = "Jättegod!"
+            };
+
+            Tomat tomat = new Tomat
+            {
+                Färg = "Röd",
+                Vikt = .75,
+                Storlek = 40,
+                Smak = "Söt men umami"
+            };
+
+            Banan banan = new Banan
+            {
+                Färg = "Gul",
+                Vikt = .50,
+                Storlek = .75,
+                Smak = "söt"
+            };
+
+            frukskål.Add(banan);
+            frukskål.Add(tomat);
+            frukskål.Add(apelsin);
+
+        }
+    }
+}
